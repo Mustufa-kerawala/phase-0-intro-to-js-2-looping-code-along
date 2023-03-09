44e35ee8f9b3bcbf6af28d0cfb6b6030be386d5b
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-09 20:33:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array, eventName ) {
+    const newArray = [];
+    for (let index = 0; index < array.length; index++) {
+         newArray[index] = `Thank you, ${array[index]}, for the wonderful surprise gift!`;        
+    }
+
+    return newArray;
+}
+
+
+function countDown(n) {
+    while(n >= 0){
+        console.log(n);
+        n--;
+    }
+
+}
+
